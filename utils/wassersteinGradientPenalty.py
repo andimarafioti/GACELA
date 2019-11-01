c306312b46f3f7de8385732fa7f62cae196bbb75
filsino <andimarafioti@gmail.com>
2019-11-01 17:09:30 +0100

    simplified the gradient penalty code

--- utils/wassersteinGradientPenalty.py
+++ utils/wassersteinGradientPenalty.py
@@ -73,8 +73,6 @@ def calc_gradient_penalty_bayes(discriminator, real_data, fake_data, gamma):
     gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
                               grad_outputs=torch.ones(disc_interpolates.size()).to(device),
                               create_graph=True, retain_graph=True, only_inputs=True)[0]
-    gradients = gradients.view(gradients.size(0), -1)
-
-    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * gamma
+    gradient_penalty = ((gradients.norm(2, dim=[1,2,3]) - 1) ** 2).mean() * gamma
 
     return gradient_penalty
