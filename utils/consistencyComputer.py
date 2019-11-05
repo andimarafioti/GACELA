9d3a4b686e5c5f42ee1e56f5d2756ee4193da950
amarafioti <andimarafioti@gmail.com>
2019-11-05 13:54:34 +0100

    Compute consistencies

---
+++ utils/consistencyComputer.py
@@ -0,0 +1,21 @@
+import torch
+import numpy as np
+
+__author__ = 'Andres'
+
+
+def substractMeanAndDivideByStd(aDistribution):
+    unmeaned = aDistribution - torch.mean(aDistribution, dim=(1, 2), keepdim=True)
+    shiftedtt = unmeaned / torch.sqrt(torch.sum(torch.abs(unmeaned) ** 2, dim=(1, 2), keepdim=True))
+    return shiftedtt
+
+
+def consistency(spectrogram):
+    ttderiv = spectrogram[:, 1:-1, :-2] - 2 * spectrogram[:, 1:-1, 1:-1] + spectrogram[:, 1:-1, 2:] + np.pi / 4
+    ffderiv = spectrogram[:, :-2, 1:-1] - 2 * spectrogram[:, 1:-1, 1:-1] + spectrogram[:, 2:, 1:-1] + np.pi / 4
+
+    absttderiv = substractMeanAndDivideByStd(torch.abs(ttderiv))
+    absffderiv = substractMeanAndDivideByStd(torch.abs(ffderiv))
+
+    consistencies = torch.sum(absttderiv * absffderiv, dim=(1, 2))
+    return consistencies
