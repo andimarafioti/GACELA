ba6ddf86359e8e74cf5cd805480f6ba030eb12d7
amarafioti <andimarafioti@gmail.com>
2019-11-14 19:43:28 +0100

    Added experiment where the generator uses log convs

---
+++ main_genLogConv.py
@@ -0,0 +1,136 @@
+import torch
+
+from ganSystemGenLogConv import GANSystemGenLogConv
+
+torch.cuda.init()
+
+from data.trainDataset import TrainDataset
+import logging
+
+# logging.getLogger().setLevel(logging.DEBUG)  # set root logger to debug
+
+"""Just so logging works..."""
+formatter = logging.Formatter('%(name)s:%(levelname)s:%(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+logging.getLogger().addHandler(console_handler)
+"""Just so logging works..."""
+
+__author__ = 'Andres'
+
+
+signal_split = [192, 128, 192]
+md = 32
+
+params_discriminator = dict()
+params_discriminator['stride'] = [2,2,2,2,2]
+params_discriminator['nfilter'] = [md, 2*md, 4*md, 8*md, 16*md]
+params_discriminator['shape'] = [[5, 5], [5, 5], [5, 5], [5, 5], [5, 5]]
+params_discriminator['full'] = []
+params_discriminator['minibatch_reg'] = False
+params_discriminator['summary'] = True
+params_discriminator['data_size'] = 2
+params_discriminator['apply_phaseshuffle'] = True
+params_discriminator['spectral_norm'] = True
+
+params_generator = dict()
+params_generator['stride'] = [2, 2, 2, 2, 2]
+params_generator['latent_dim'] = 100
+params_generator['nfilter'] = [8*md, 4*md, 2*md, md, 1]
+params_generator['shape'] = [[4, 4], [4, 4], [8, 8], [8, 8], [8, 8]]
+params_generator['padding'] = [[1, 1], [1, 1], [3, 3], [3, 3], [3, 3]]
+params_generator['full'] = 256*md
+params_generator['summary'] = True
+params_generator['data_size'] = 2
+params_generator['spectral_norm'] = True
+params_generator['in_conv_shape'] = [8, 4]
+params_generator['borders'] = dict()
+params_generator['borders']['nfilter'] = [md, 2*md, md, md/2]
+params_generator['borders']['shape'] = [[5, 5],[5, 5],[5, 5],[5, 5]]
+params_generator['borders']['stride'] = [2, 2, 3, 4]
+params_generator['borders']['data_size'] = 2
+params_generator['borders']['border_scale'] = 1
+
+
+# Optimization parameters inspired from 'Self-Attention Generative Adversarial Networks'
+# - Spectral normalization GEN DISC
+# - Batch norm GEN
+# - TTUR ('GANs Trained by a Two Time-Scale Update Rule Converge to a Local Nash Equilibrium')
+# - ADAM  beta1=0 beta2=0.9, disc lr 0.0004, gen lr 0.0001
+# - Hinge loss
+# Parameters are similar to the ones in those papers...
+# - 'PROGRESSIVE GROWING OF GANS FOR IMPROVED QUALITY, STABILITY, AND VARIATION'
+# - 'LARGE SCALE GAN TRAINING FOR HIGH FIDELITY NATURAL IMAGE SYNTHESIS'
+# - 'CGANS WITH PROJECTION DISCRIMINATOR'
+
+params_optimization = dict()
+params_optimization['batch_size'] = 64*2
+params_discriminator['batch_size'] = 64*2
+
+params_optimization['epoch'] = 600
+params_optimization['n_critic'] = 1
+params_optimization['generator'] = dict()
+params_optimization['generator']['optimizer'] = 'adam'
+params_optimization['generator']['kwargs'] = [0.5, 0.9]
+params_optimization['generator']['learning_rate'] = 1e-4
+params_optimization['discriminator'] = dict()
+params_optimization['discriminator']['optimizer'] = 'adam'
+params_optimization['discriminator']['kwargs'] = [0.5, 0.9]
+params_optimization['discriminator']['learning_rate'] = 1e-4
+
+# all parameters
+params = dict()
+params['net'] = dict() # All the parameters for the model
+params['net']['generator'] = params_generator
+params['net']['discriminator'] = params_discriminator
+params['net']['prior_distribution'] = 'gaussian'
+params['net']['shape'] = [1, 256, 128*4] # Shape of the image
+params['net']['inpainting']=dict()
+params['net']['inpainting']['split']=signal_split
+params['net']['gamma_gp'] = 10 # Gradient penalty
+# params['net']['fs'] = 16000//downscale
+params['net']['loss_type'] ='wasserstein'
+
+params['optimization'] = params_optimization
+params['summary_every'] = 250 # Tensorboard summaries every ** iterations
+params['print_every'] = 50 # Console summaries every ** iterations
+params['save_every'] = 1000 # Save the model every ** iterations
+# params['summary_dir'] = os.path.join(global_path, name +'_summary/')
+# params['save_dir'] = os.path.join(global_path, name + '_checkpoints/')
+params['Nstats'] = 500
+
+args = dict()
+args['generator'] = params_generator
+args['discriminator_count'] = 3
+args['discriminator'] = params_discriminator
+args['borderEncoder'] = params_generator['borders']
+args['discriminator_in_shape'] = [1, 256, 128]
+args['generator_input'] = 2*6*4*2*8+24*4
+args['optimizer'] = params_optimization
+args['split'] = signal_split
+args['log_interval'] = 50
+args['spectrogram_shape'] = params['net']['shape']
+args['gamma_gp'] = params['net']['gamma_gp']
+args['tensorboard_interval'] = 250
+args['save_path'] = '../saved_results/'
+args['experiment_name'] = 'pytorch_nc1_pow2loss_2'
+args['save_interval'] = 1000
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+examples_per_file = 16
+trainDataset = TrainDataset("../data/Maestro_spectrograms_mep/", window_size=512, examples_per_file=examples_per_file)
+
+train_loader = torch.utils.data.DataLoader(trainDataset,
+    batch_size=args['optimizer']['batch_size']//examples_per_file, shuffle=True,
+                                           num_workers=4, drop_last=True)
+start_at_step = 0
+start_at_epoch = 0
+
+ganSystem = GANSystemGenLogConv(args)
+for epoch in range(start_at_epoch, 10):
+    start_at_step, can_restart = ganSystem.train(train_loader, epoch, start_at_step)
+    if not can_restart:
+        break
---
+++ model/generatorLogConv.py
@@ -0,0 +1,100 @@
+import torch
+from torch import nn
+
+from model.borderEncoder import BorderEncoder
+from model.logConv import LogConv
+
+__author__ = 'Andres'
+
+
+class GeneratorLogConv(nn.Module):
+    """Not using noise for now, since the border conditioning might already be enough"""
+    def __init__(self, params, in_shape):
+        super(GeneratorLogConv, self).__init__()
+
+        self._params = params
+        self.linGenerator = nn.ModuleList([nn.Sequential(nn.utils.weight_norm(nn.Linear(in_shape, self._params['full'])))])
+
+        curr_channel_count = int(self._params['full'] / self._params['in_conv_shape'][0] /
+                                 self._params['in_conv_shape'][1])
+        logSize = self._params['in_conv_shape'][0]
+
+        self.convGenerator = nn.ModuleList()
+
+        for nfilters, kernel_shape, stride, padding in zip(self._params['nfilter'][:-1], self._params['shape'][:-1],
+                                                  self._params['stride'][:-1], self._params['padding'][:-1]):
+            self.convGenerator.append(nn.Sequential(
+                LogConv(log_size=logSize, in_channels=curr_channel_count, out_channels=nfilters,
+                                               kernel_size=kernel_shape, stride=1/stride,
+                                               padding=padding),
+                nn.ReLU(),
+            ))
+            curr_channel_count = nfilters
+            logSize = logSize*stride
+
+        self.convGenerator.append(nn.Sequential(
+            LogConv(log_size=logSize, in_channels=curr_channel_count, out_channels=self._params['nfilter'][-1],
+                                           kernel_size=self._params['shape'][-1], stride=self._params['stride'][-1],
+                                           padding=self._params['padding'][-1]),
+            nn.Tanh(),
+        ))
+
+
+    def forward_lin(self, x):
+        for module in self.linGenerator:
+            x = module(x)
+        return x
+
+    def forward_conv(self, x):
+        for module in self.convGenerator:
+            x = module(x)
+        return x
+
+    def forward(self, x):
+        x = x.view(x.size()[0], -1)
+        x = self.forward_lin(x)
+        conv_input_channels = self._params['full'] / self._params['in_conv_shape'][0] / self._params['in_conv_shape'][1]
+        x = x.view(x.size()[0], int(conv_input_channels), *self._params['in_conv_shape'])
+        x = self.forward_conv(x)
+        return x
+
+
+if __name__ == '__main__':
+    params_borders = dict()
+    md = 32
+    input_shape = [1, 256, 192]
+    batch_size = 64
+
+    params_borders['nfilter'] = [md, 2 * md, md, md // 2]
+    params_borders['shape'] = [[5, 5], [5, 5], [5, 5], [5, 5]]
+    params_borders['stride'] = [2, 2, 3, 4]
+    params_borders['data_size'] = 2
+
+    model = BorderEncoder(params_borders)
+    print(model)
+
+    x = torch.randn(batch_size, *input_shape)
+    print(x.shape)
+
+    score = model(x)
+
+    print(score.shape)
+
+    params_generator = dict()
+    params_generator['stride'] = [2, 2, 2, 2, 2]
+    params_generator['latent_dim'] = 100
+    params_generator['nfilter'] = [8 * md, 4 * md, 2 * md, md, 1]
+    params_generator['shape'] = [[4, 4], [4, 4], [8, 8], [8, 8], [8, 8]]
+    params_generator['padding'] = [[1, 1], [1, 1], [3, 3], [3, 3], [3, 3]]
+    params_generator['full'] = 256 * md
+    params_generator['summary'] = True
+    params_generator['data_size'] = 2
+    params_generator['spectral_norm'] = True
+    params_generator['in_conv_shape'] = [8, 4]
+
+    model = Generator(params_generator, score.shape[1]*score.shape[2]*score.shape[3]*2)
+    print(model)
+
+    score = model(torch.cat((score, score), 1))
+
+    print(score.shape)
