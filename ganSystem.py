d2985c8fbb0c945ad03e89787a8ca4d18de71231
amarafioti <andimarafioti@gmail.com>
2020-02-13 16:31:14 +0100

    adapt to mel discriminators

--- ganSystem.py
+++ ganSystem.py
@@ -1,5 +1,5 @@
 import torch
-from tifresi.transforms import inv_log_spectrogram
+from tifresi.transforms import inv_log_spectrogram, mel_spectrogram
 from torch import nn
 import torch.nn.functional as F
 
@@ -21,9 +21,13 @@ class GANSystem(object):
 		super(GANSystem, self).__init__()
 		self.args = args
 		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-		self.discriminators = nn.ModuleList(
-			[Discriminator(args['discriminator'], args['discriminator_in_shape'])
-			 for _ in range(args['discriminator_count'])]).to(device)
+		self.stft_discriminators = nn.ModuleList(
+			[Discriminator(args['discriminator'], args['stft_discriminator_in_shape'])
+			 for _ in range(args['stft_discriminator_count'])]).to(device)
+
+		self.mel_discriminators = nn.ModuleList(
+			[Discriminator(args['discriminator'], args['mel_discriminator_in_shape'])
+			 for _ in range(args['mel_discriminator_count'])]).to(device)
 
 		self.left_border_encoder = BorderEncoder(args['borderEncoder']).to(device)
 		self.right_border_encoder = BorderEncoder(args['borderEncoder']).to(device)
@@ -34,9 +38,12 @@ class GANSystem(object):
 								list(self.right_border_encoder.parameters()),
 								lr=args['optimizer']['generator']['learning_rate'],
 								betas=(0.5, 0.9))
-		self.optims_d = [torch.optim.Adam(discriminator.parameters(),
+		self.stft_optims_d = [torch.optim.Adam(discriminator.parameters(),
 								lr=args['optimizer']['discriminator']['learning_rate'],
-								betas=(0.5, 0.9)) for discriminator in self.discriminators]
+								betas=(0.5, 0.9)) for discriminator in self.stft_discriminators]
+		self.mel_optims_d = [torch.optim.Adam(discriminator.parameters(),
+								lr=args['optimizer']['discriminator']['learning_rate'],
+								betas=(0.5, 0.9)) for discriminator in self.mel_discriminators]
 
 		self.model_saver = TorchModelSaver(args['experiment_name'], args['save_path'])
 		self._spectrogramInverter = SpectrogramInverter(args['fft_length'], args['fft_hop_size'])
@@ -46,6 +53,13 @@ class GANSystem(object):
 
 	def loadModel(self, batch_idx, epoch):
 		self.model_saver.loadModel(self, batch_idx, epoch)
+
+	def time_average(self, matrix_batch, reduction_rate):
+		tmp = torch.zeros([matrix_batch.shape[0], matrix_batch.shape[1], matrix_batch.shape[2], matrix_batch.shape[3] // reduction_rate]).float().cuda()
+		for i in range(reduction_rate):
+			tmp += matrix_batch[:, :, :, i::reduction_rate]
+		matrix_batch = tmp / reduction_rate
+		return matrix_batch
 
 	def train(self, train_loader, epoch, batch_idx=0):
 		self.summarizer = TensorboardSummarizer(self.args['save_path'] + self.args['experiment_name'] + '_summary',
@@ -71,24 +85,26 @@ class GANSystem(object):
 				real_spectrograms = data[::2]
 				fake_left_borders = data[1::2, :, :, :self.args['split'][0]]
 				fake_right_borders = data[1::2, :, :, self.args['split'][0] + self.args['split'][1]:]
+				encoded_left_border = self.left_border_encoder(fake_left_borders)
+				encoded_right_border = self.right_border_encoder(fake_right_borders)
+				encoded_size = encoded_left_border.size()
+				noise = torch.rand(encoded_size[0], 4, encoded_size[2], encoded_size[3]).to(device)
+				generated_spectrograms = self.generator(
+					torch.cat((encoded_left_border, encoded_right_border, noise), 1))
 
-				# optimize D
+				fake_spectrograms = torch.cat((fake_left_borders, generated_spectrograms, fake_right_borders), 3)
+
+				# optimize stft_D
 				for _ in range(self.args['optimizer']['n_critic']):
-					for index, (discriminator, optim_d) in enumerate(zip(self.discriminators, self.optims_d)):
+					for index, (discriminator, optim_d) in enumerate(zip(self.stft_discriminators, self.stft_optims_d)):
 						optim_d.zero_grad()
-						encoded_left_border = self.left_border_encoder(fake_left_borders)
-						encoded_right_border = self.right_border_encoder(fake_right_borders)
-						encoded_size = encoded_left_border.size()
-						noise = torch.rand(encoded_size[0], 4, encoded_size[2], encoded_size[3]).to(device)
-						generated_spectrograms = self.generator(torch.cat((encoded_left_border, encoded_right_border, noise), 1))
-
-						fake_spectrograms = torch.cat((fake_left_borders, generated_spectrograms, fake_right_borders), 3)
 						scale = 2 ** index
-						time_axis = self.args['spectrogram_shape'][2]
-						start = int((time_axis - (time_axis // (2**(len(self.discriminators)-1))) * scale) / 2)
-						end = time_axis - start
-						x_fake = fake_spectrograms[:, :, :, start:end:scale].detach()
-						x_real = real_spectrograms[:, :, :, start:end:scale].detach()
+						signal_length = self.args['spectrogram_shape'][2]
+						gap_length = self.args['split'][1]
+						start = int(signal_length//2 - (gap_length//2) * scale)
+						end = signal_length - start
+						x_fake = self.time_average(fake_spectrograms[:, :, :, start:end], scale).detach()
+						x_real = self.time_average(real_spectrograms[:, :, :, start:end], scale).detach()
 
 						d_loss_f = discriminator(x_fake).mean()
 						d_loss_r = discriminator(x_real).mean()
@@ -105,6 +121,35 @@ class GANSystem(object):
 						disc_loss.backward()
 						optim_d.step()
 
+					# optimize mel_D
+					for _ in range(self.args['optimizer']['n_critic']):
+						for index, (discriminator, optim_d) in enumerate(
+								zip(self.mel_discriminators, self.mel_optims_d), self.args['mel_discriminator_start_powscale']):
+							optim_d.zero_grad()
+							scale = 2 ** index
+							signal_length = self.args['spectrogram_shape'][2]
+							gap_length = self.args['split'][1]
+							start = int(signal_length // 2 - (gap_length // 2) * scale)
+							end = signal_length - start
+
+							x_fake = self.time_average(mel_spectrogram(fake_spectrograms[:, :, :, start:end]), scale).detach()
+							x_real = self.time_average(mel_spectrogram(real_spectrograms[:, :, :, start:end]), scale).detach()
+
+							d_loss_f = discriminator(x_fake).mean()
+							d_loss_r = discriminator(x_real).mean()
+
+							grad_pen = calc_gradient_penalty_bayes(discriminator, x_real, x_fake, self.args['gamma_gp'])
+							d_loss_gp = grad_pen.mean()
+							disc_loss = d_loss_f - d_loss_r + d_loss_gp
+
+							self.summarizer.trackScalar("Disc{:1d}/Loss".format(int(index)), disc_loss)
+							self.summarizer.trackScalar("Disc{:1d}/GradPen".format(int(index)), d_loss_gp)
+							self.summarizer.trackScalar("Disc{:1d}/Loss_f".format(int(index)), d_loss_f)
+							self.summarizer.trackScalar("Disc{:1d}/Loss_r".format(int(index)), d_loss_r)
+
+							disc_loss.backward()
+							optim_d.step()
+
 				# optimize G
 
 				self.optim_g.zero_grad()
@@ -118,14 +163,27 @@ class GANSystem(object):
 				fake_spectrograms = torch.cat((fake_left_borders, generated_spectrograms, fake_right_borders), 3)
 				gen_loss = 0
 
-				for index, discriminator in enumerate(self.discriminators):
+				for index, discriminator in enumerate(self.stft_discriminators):
 					scale = 2 ** index
-					time_axis = self.args['spectrogram_shape'][2]
-					start = int((time_axis - (time_axis // (2**(len(self.discriminators)-1))) * scale) / 2)
-					end = time_axis - start
-					x_fake = fake_spectrograms[:, :, :, start:end:scale]
+					signal_length = self.args['spectrogram_shape'][2]
+					gap_length = self.args['split'][1]
+					start = int(signal_length // 2 - (gap_length // 2) * scale)
+					end = signal_length - start
+					x_fake = self.time_average(fake_spectrograms[:, :, :, start:end], scale).detach()
 
-					d_loss_f = discriminator(x_fake)
+					d_loss_f = discriminator(x_fake).mean()
+					gen_loss += - d_loss_f.mean()
+
+				for index, discriminator in enumerate(
+						self.mel_discriminators, self.args['mel_discriminator_start_powscale']):
+					signal_length = self.args['spectrogram_shape'][2]
+					gap_length = self.args['split'][1]
+					start = int(signal_length // 2 - (gap_length // 2) * scale)
+					end = signal_length - start
+
+					x_fake = self.time_average(mel_spectrogram(fake_spectrograms[:, :, :, start:end]), scale).detach()
+
+					d_loss_f = discriminator(x_fake).mean()
 					gen_loss += - d_loss_f.mean()
 
 				self.summarizer.trackScalar("Gen/Loss", gen_loss)
--- main_largerContext.py
+++ main_largerContext.py
@@ -17,36 +17,34 @@ logging.getLogger().addHandler(console_handler)
 
 __author__ = 'Andres'
 
-
-signal_split = [448, 128, 448]
+signal_split = [480, 64, 480]
 md = 32
 
 params_discriminator = dict()
-params_discriminator['stride'] = [1, 1, 2, 2, 1]
-params_discriminator['nfilter'] = [md, md, 2 * md, 4 * md, 1]
+params_discriminator['stride'] = [2, 2, 2, 2, 2]
+params_discriminator['nfilter'] = [md, 2 * md, 4 * md, 8 * md, 16 * md]
 params_discriminator['shape'] = [[5, 5], [5, 5], [5, 5], [5, 5], [5, 5]]
 params_discriminator['data_size'] = 2
 
 params_generator = dict()
 params_generator['stride'] = [2, 2, 2, 2, 2]
-params_generator['nfilter'] = [4 * md, 4 * md, 2 * md, md, 1]
-params_generator['shape'] = [5, 5, 7, 7, 7]
-params_generator['padding'] = [2, 2, 3, 3, 3]
-# params_generator['residual_blocks'] = 2
+params_generator['nfilter'] = [8 * md, 4 * md, 2 * md, md, 1]
+params_generator['shape'] = [[4, 4], [4, 4], [8, 8], [8, 8], [8, 8]]
+params_generator['padding'] = [[1, 1], [1, 1], [3, 3], [3, 3], [3, 3]]
+params_generator['residual_blocks'] = 2
 
-params_generator['full'] = 256*md
+params_generator['full'] = 256 * md
 params_generator['summary'] = True
 params_generator['data_size'] = 2
-params_generator['in_conv_shape'] = [16, 4]
+params_generator['in_conv_shape'] = [16, 2]
 params_generator['borders'] = dict()
-params_generator['borders']['nfilter'] = [md, 2*md, md, md/2]
-params_generator['borders']['shape'] = [[5, 5],[5, 5],[5, 5],[5, 5]]
+params_generator['borders']['nfilter'] = [md, 2 * md, md, md / 2]
+params_generator['borders']['shape'] = [[5, 5], [5, 5], [5, 5], [5, 5]]
 params_generator['borders']['stride'] = [2, 2, 3, 4]
 params_generator['borders']['data_size'] = 2
 params_generator['borders']['border_scale'] = 2
 # This does not work because of flipping, border 2 need to be flipped tf.reverse(l, axis=[1]), ask Nathanael
 params_generator['borders']['width_full'] = None
-
 
 # Optimization parameters inspired from 'Self-Attention Generative Adversarial Networks'
 # - Spectral normalization GEN DISC
@@ -60,8 +58,8 @@ params_generator['borders']['width_full'] = None
 # - 'CGANS WITH PROJECTION DISCRIMINATOR'
 
 params_optimization = dict()
-params_optimization['batch_size'] = 64*2
-params_discriminator['batch_size'] = 64*2
+params_optimization['batch_size'] = 64
+params_discriminator['batch_size'] = 64
 
 params_optimization['n_critic'] = 1
 params_optimization['generator'] = dict()
@@ -95,20 +93,23 @@ params['save_every'] = 1000  # Save the model every ** iterations
 
 args = dict()
 args['generator'] = params_generator
-args['discriminator_count'] = 4
+args['stft_discriminator_count'] = 2
+args['mel_discriminator_count'] = 3
 args['discriminator'] = params_discriminator
 args['borderEncoder'] = params_generator['borders']
-args['discriminator_in_shape'] = [1, 512, 128]
-args['generator_input'] = 1980
+args['stft_discriminator_in_shape'] = [1, 512, 64]
+args['mel_discriminator_in_shape'] = [1, 80, 64]
+args['mel_discriminator_start_powscale'] = 2
+args['generator_input'] = 1188*2
 args['optimizer'] = params_optimization
 args['split'] = signal_split
-args['log_interval'] = 50
+args['log_interval'] = 100
 args['spectrogram_shape'] = params['net']['shape']
 args['gamma_gp'] = params['net']['gamma_gp']
-args['tensorboard_interval'] = 250
+args['tensorboard_interval'] = 500
 args['save_path'] = 'saved_results/'
-args['experiment_name'] = 'pytorch_nc1_largerContext'
-args['save_interval'] = 1000
+args['experiment_name'] = 'wasserstein_old_params_structures_96_64_nd3_2skip'
+args['save_interval'] = 50000
 
 args['fft_length'] = 1024
 args['fft_hop_size'] = 256
@@ -116,15 +117,17 @@ args['sampling_rate'] = 22050
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-examples_per_file = 16
+examples_per_file = 32
 audioLoader = AudioLoader(args['sampling_rate'], args['fft_length'], args['fft_hop_size'], 50)
 
 dataFolder = "../../../Datasets/maestro-v2.0.0/"
 
-trainDataset = TrainDataset(dataFolder, window_size=1024, audio_loader=audioLoader, examples_per_file=examples_per_file)
+trainDataset = TrainDataset(dataFolder, window_size=1024, audio_loader=audioLoader, examples_per_file=examples_per_file,
+                            loaded_files_buffer=20, file_usages=30)
 
 train_loader = torch.utils.data.DataLoader(trainDataset,
-    batch_size=args['optimizer']['batch_size']//examples_per_file, shuffle=True,
+                                           batch_size=args['optimizer']['batch_size'] // examples_per_file,
+                                           shuffle=True,
                                            num_workers=4, drop_last=True)
 
 start_at_step = 0
